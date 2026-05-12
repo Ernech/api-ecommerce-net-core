28001272209a46e107c1f44c980f74f541454eb0
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2026-05-11 22:21:43 -0400

    MODELO PRODUCTO

---
+++ ApiEcommerce/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiEcommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [Range(0,double.MaxValue)]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string SKU { get; set; } = string.Empty; //PROD-001-BLK-M
+
+        [Range(0,int.MaxValue)]
+        public int Stock { get; set; }
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdateDate { get; set; } = null;
+
+        //Relación con el modelo category
+        public int CategoryId { get; set; }
+
+        [ForeignKey("Id")]
+        public required Category Category { get; set; }
+    }
+}
