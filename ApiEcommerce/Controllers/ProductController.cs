724ad72b7bcfd392cb4098991edb05f7b25a1562
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2026-05-19 23:18:47 -0400

    PRODUCT CONTROLLER

---
+++ ApiEcommerce/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using ApiEcommerce.Models;
+using ApiEcommerce.Models.DTO;
+using ApiEcommerce.Repository;
+using ApiEcommerce.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository,IMapper mapper) : ControllerBase
+    {
+        private readonly IProductRepository _productRepository = productRepository;
+        private readonly ICategoryRepository _categoryRepository = categoryRepository;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetProducts();
+            var productsDTO = _mapper.Map<List<ProductDTO>>(products);
+            
+            return Ok(productsDTO);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _productRepository.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound($"EL producto con el id {id} no existe");
+            }
+            var productDTO = _mapper.Map<CategoryDTO>(product);
+
+            return Ok(productDTO);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateProduct([FromBody] CreateProductDTO createProductDTO)
+        {
+            if (createProductDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (_productRepository.ProductExists(createProductDTO.Name))
+            {
+                ModelState.AddModelError("CustomError", "El producto ya existe");
+                return BadRequest(ModelState);
+            }
+            if (!_categoryRepository.CategoryExists(createProductDTO.CategoryId))
+            {
+                ModelState.AddModelError("CustomError", "La categoria no existe");
+                return BadRequest(ModelState);
+            }
+            var product = _mapper.Map<Product>(createProductDTO);
+            if (!_productRepository.CreateProduct(product))
+            {
+                ModelState.AddModelError("CustomError", $"Algo salió mal al guardar el registro {product.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetProduct", new { id = product.ProductId }, product);
+        }
+
+
+
+
+
+    }
+}
--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -9,6 +9,7 @@ var dbConectionString = builder.Configuration.GetConnectionString("ConexionSql")
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConectionString));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(cfg => 
 { 
     cfg.AddMaps(typeof(Program).Assembly); 
