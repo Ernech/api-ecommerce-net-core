a8da2d21a6fba5894d9ef3dd2c513e7841f91fe5
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2026-06-03 00:48:33 -0400

    IMPLEMENTACIÓN DEL LOGIN DE USUARIO

--- ApiEcommerce/Repository/UserRepository.cs
+++ ApiEcommerce/Repository/UserRepository.cs
@@ -2,12 +2,18 @@
 using ApiEcommerce.Models;
 using ApiEcommerce.Models.DTO;
 using ApiEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ApiEcommerce.Repository
 {
-    public class UserRepository(ApplicationDbContext dbContext) : IUserRepository
+    public class UserRepository(ApplicationDbContext dbContext,IConfiguration configuration) : IUserRepository
     {
         private readonly ApplicationDbContext _dbContext = dbContext;
+        private string? secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
         public User? GetUser(int id)
         {
             return _dbContext.Users.FirstOrDefault(u=>u.Id==id);
@@ -23,9 +29,77 @@ namespace ApiEcommerce.Repository
             return !_dbContext.Users.Any(u => u.Username.ToLower().Trim() == username.ToLower().Trim());
         }
 
-        public Task<UserLoginResponseDTO> Login(UserLoginDTO userLoginDTO)
+        public async Task<UserLoginResponseDTO> Login(UserLoginDTO userLoginDTO)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(userLoginDTO.Username))
+            {
+                return new UserLoginResponseDTO()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "El username es requerido"
+                };
+            }
+            var user = await _dbContext.Users.FirstOrDefaultAsync<User>(u=>u.Username.ToLower().Trim()==userLoginDTO.Username.ToLower().Trim());
+            if (user == null)
+            {
+                return new UserLoginResponseDTO()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "Usuario no encontrado"
+                };
+            }
+            if (user ==null)
+            {
+                return new UserLoginResponseDTO()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "Usuario no encontrado"
+                };
+            }
+            if (!BCrypt.Net.BCrypt.Verify(userLoginDTO.Password,user.Password))
+            {
+                return new UserLoginResponseDTO()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "Las credenciales son incorrectas"
+                };
+            }
+            var handlerToken = new JwtSecurityTokenHandler();
+            if (string.IsNullOrWhiteSpace(secretKey))
+            {
+                throw new InvalidOperationException("Secret key no configurada");
+            }
+            var key = Encoding.UTF8.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                [
+                    new Claim("id",user.Id.ToString()),
+                    new Claim("username",user.Username),
+                    new Claim(ClaimTypes.Role,user.Role??string.Empty)
+
+                ]),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = handlerToken.CreateToken(tokenDescriptor);
+            return new UserLoginResponseDTO() 
+            {
+                Message="Login exitoso",
+                Token = handlerToken.WriteToken(token),
+                User = new UserRegisterDTO 
+                {
+                    Name = user.Name,
+                    Password=user.Password?? string.Empty,
+                    Username = user.Username,
+                    Role = user.Role ??string.Empty
+                }
+            };
+
         }
 
         public async Task<User> Register(UserRegisterDTO userRegisterDTO)
